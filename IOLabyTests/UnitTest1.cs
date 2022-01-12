dbcf7dc1941d29164b1231dfb769fd55c9f14b8f
kdt111 <j.malek@onet.eu>
2022-01-12 20:43:07 +0100

    Unit tests fixed

---
+++ IOLabyTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IOLaby.Data;
+
+namespace IOLabyTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Database database = new Database();
+		}
+	}
+}
